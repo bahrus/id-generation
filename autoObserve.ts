9be2c60782b7d2c2e33a0b6dec47cf7e16f13160
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-01-28 16:46:02 -0500

    Initial commit

--- autoObserve.ts
+++ autoObserve.ts
@@ -20,20 +20,7 @@ export function autoObserve(rootNode: Node = document, signal?: AbortSignal): Mo
     });
     
     observer.observe(rootNode);
-    
-    // // Also process any existing elements with -id that are already in the DOM
-    // if ('querySelectorAll' in rootNode && typeof (rootNode as any).querySelectorAll === 'function') {
-    //     const allElements = (rootNode as ParentNode).querySelectorAll('*');
-    //     for (const element of allElements) {
-    //         if (element.hasAttribute('-id')) {
-    //             const scope = element.closest('fieldset,[itemscope]') || rootNode;
-    //             if (scope instanceof Element || scope instanceof DocumentFragment || 
-    //                 (typeof ShadowRoot !== 'undefined' && scope instanceof ShadowRoot)) {
-    //                 genIds(scope);
-    //             }
-    //         }
-    //     }
-    // }
+
     
     return observer;
 }
